d78b93b3b2c46c0974352e46779062425e1c2fba
Joel Powers <joelpowers@gmail.com>
2015-11-03 19:54:13 -0600

    Took sample code - three object in a scene - tap on them and they disappear.  Works in simulator and when uploaded to iPad.

--- swift-pipes/GameViewController.swift
+++ swift-pipes/GameViewController.swift
@@ -11,59 +11,85 @@ import QuartzCore
 import SceneKit
 
 class GameViewController: UIViewController {
+    
+    var sceneView: SCNView!
+    var camera: SCNNode!
+    var ground: SCNNode!
+    var light: SCNNode!
+    var button: SCNNode!
+    var sphere1: SCNNode!
+    var sphere2: SCNNode!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        sceneView = SCNView(frame: self.view.frame)
+        sceneView.scene = SCNScene()
+        self.view.addSubview(sceneView)
         
-        // create and add a camera to the scene
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
-        scene.rootNode.addChildNode(cameraNode)
+        let tapRecognizer = UITapGestureRecognizer()
+        tapRecognizer.numberOfTapsRequired = 1
+        tapRecognizer.numberOfTouchesRequired = 1
+        tapRecognizer.addTarget(self, action: "sceneTapped:")
+        sceneView.gestureRecognizers = [tapRecognizer]
         
-        // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
         
-        // create and add a light to the scene
-        let lightNode = SCNNode()
-        lightNode.light = SCNLight()
-        lightNode.light!.type = SCNLightTypeOmni
-        lightNode.position = SCNVector3(x: 0, y: 10, z: 10)
-        scene.rootNode.addChildNode(lightNode)
+        let groundGeometry = SCNFloor()
+        groundGeometry.reflectivity = 0
+        let groundMaterial = SCNMaterial()
+        groundMaterial.diffuse.contents = UIColor.blueColor()
+        groundGeometry.materials = [groundMaterial]
+        ground = SCNNode(geometry: groundGeometry)
         
-        // create and add an ambient light to the scene
-        let ambientLightNode = SCNNode()
-        ambientLightNode.light = SCNLight()
-        ambientLightNode.light!.type = SCNLightTypeAmbient
-        ambientLightNode.light!.color = UIColor.darkGrayColor()
-        scene.rootNode.addChildNode(ambientLightNode)
+        let camera = SCNCamera()
+        camera.zFar = 10000
+        self.camera = SCNNode()
+        self.camera.camera = camera
+        self.camera.position = SCNVector3(x: -20, y: 15, z: 20)
+        let constraint = SCNLookAtConstraint(target: ground)
+        constraint.gimbalLockEnabled = true
+        self.camera.constraints = [constraint]
         
-        // retrieve the ship node
-        let ship = scene.rootNode.childNodeWithName("ship", recursively: true)!
+        let ambientLight = SCNLight()
+        ambientLight.color = UIColor.darkGrayColor()
+        ambientLight.type = SCNLightTypeAmbient
+        self.camera.light = ambientLight
         
-        // animate the 3d object
-        ship.runAction(SCNAction.repeatActionForever(SCNAction.rotateByX(0, y: 2, z: 0, duration: 1)))
+        let spotLight = SCNLight()
+        spotLight.type = SCNLightTypeSpot
+        spotLight.castsShadow = true
+        spotLight.spotInnerAngle = 70.0
+        spotLight.spotOuterAngle = 90.0
+        spotLight.zFar = 500
+        light = SCNNode()
+        light.light = spotLight
+        light.position = SCNVector3(x: 0, y: 25, z: 25)
+        light.constraints = [constraint]
         
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
+        let sphereGeometry = SCNSphere(radius: 1.5)
+        let sphereMaterial = SCNMaterial()
+        sphereMaterial.diffuse.contents = UIColor.greenColor()
+        sphereGeometry.materials = [sphereMaterial]
         
-        // set the scene to the view
-        scnView.scene = scene
+        sphere1 = SCNNode(geometry: sphereGeometry)
+        sphere1.position = SCNVector3(x: -15, y: 1.5, z: 0)
+        sceneView.scene?.rootNode.addChildNode(sphere1)
         
-        // allows the user to manipulate the camera
-        scnView.allowsCameraControl = true
+        sphere2 = SCNNode(geometry: sphereGeometry)
+        sphere2.position = SCNVector3(x: 15, y: 1.5, z: 0)
+        sceneView.scene?.rootNode.addChildNode(sphere2)
         
-        // show statistics such as fps and timing information
-        scnView.showsStatistics = true
+        let buttonGeometry = SCNBox(width: 4, height: 1, length: 4, chamferRadius: 0)
+        let buttonMaterial = SCNMaterial()
+        buttonMaterial.diffuse.contents = UIColor.redColor()
+        buttonGeometry.materials = [buttonMaterial]
+        button = SCNNode(geometry: buttonGeometry)
+        button.position = SCNVector3(x: 8, y: 0.5, z: 0)
         
-        // configure the view
-        scnView.backgroundColor = UIColor.blackColor()
-        
-        // add a tap gesture recognizer
-        let tapGesture = UITapGestureRecognizer(target: self, action: "handleTap:")
-        scnView.addGestureRecognizer(tapGesture)
+        sceneView.scene?.rootNode.addChildNode(self.camera)
+        sceneView.scene?.rootNode.addChildNode(ground)
+        sceneView.scene?.rootNode.addChildNode(light)
+        sceneView.scene?.rootNode.addChildNode(button)
     }
     
     func handleTap(gestureRecognize: UIGestureRecognizer) {
@@ -100,7 +126,17 @@ class GameViewController: UIViewController {
             SCNTransaction.commit()
         }
     }
-    
+    func sceneTapped(recognizer: UITapGestureRecognizer) {
+        let location = recognizer.locationInView(sceneView)
+        
+        let hitResults = sceneView.hitTest(location, options: nil)
+        if hitResults.count > 0 {
+            let result = hitResults[0]
+            let node = result.node
+            node.removeFromParentNode()
+        }
+    }
+
     override func shouldAutorotate() -> Bool {
         return true
     }
